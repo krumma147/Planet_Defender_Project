407b69a77bae0091dead8a1a856454c77e0e9691
Pomelo <64297316+krumma147@users.noreply.github.com>
2023-09-24 16:26:31 +0700

    fixed player movement!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,31 +6,46 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] InputAction movement;
+    [SerializeField]float controlSpeed = 25f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 5f;
+
+    [SerializeField] float pitchFactor = 2f;
+
+    float xThrow;
+    float yThrow;
     void Start()
     {
         
     }
-    private void OnEnable()
-    {
-        movement.Enable();
-
-    }
-
-    private void OnDisable()
-    {
-        movement.Disable();
-    }
     // Update is called once per frame
     void Update()
     {
-        float horizon = movement.ReadValue<Vector2>().x;
-        float verti = movement.ReadValue<Vector2>().y;
+        MovingProcess();
+        RotationProcess();
+    }
 
-        /*float horizon = Input.GetAxis("Horizontal");*/
-        Debug.Log("Horizontal value:" + horizon);
+    void MovingProcess()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
+        float offsetX = xThrow * Time.deltaTime * controlSpeed;
+        float offsetY = yThrow * Time.deltaTime * controlSpeed;
 
-        /*float vertical = Input.GetAxis("Vertical");*/
-        Debug.Log("Vertical value:" + verti);
+        float rawX = transform.localPosition.x + offsetX;
+        float rawY = transform.localPosition.y + offsetY;
+
+        float newX = Mathf.Clamp(rawX, -xRange, xRange);
+        float newY = Mathf.Clamp(rawY, -yRange, yRange);
+
+        transform.localPosition = new Vector3(newX, newY, transform.localPosition.z);
+    }
+
+    void RotationProcess()
+    {
+        float pitch = transform.localPosition.y * pitchFactor + yThrow;
+        float yaw = 0f;
+        float roll = 0f;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
